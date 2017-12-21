5b1c8bc4781f6e17ef642eb2f4302b3f0608d6f0
Shannon Code <genecyber@gmail.com>
2017-12-21 13:30:01 -0500

    lightrail and nucypher kms introduced

--- build/python/pre.py
+++ build/python/pre.py
@@ -1,4 +1,8 @@
 from npre import bbs98
+from nkms.crypto.encrypting_keypair import EncryptingKeypair
+pre = bbs98.PRE()
+''' 
+Private Rekey from npre
 pre = bbs98.PRE()
 backup = pre
 sk_a = pre.gen_priv(dtype=bytes)
@@ -12,4 +16,37 @@ emsg = pre.encrypt(pk_a, msg)
 #print(pre.decrypt(sk_b, emsg))
 re_ab = pre.rekey(sk_a, sk_b)
 emsg_b = pre.reencrypt(re_ab, emsg)
-print(pre.decrypt(sk_b, emsg_b))
+print(pre.decrypt(sk_b, emsg_b)) '''
+
+''' 
+Decrypt from nkms
+data = b'Hello world'
+alice = EncryptingKeypair()
+e = alice.encrypt(data)
+assert alice.decrypt(e) == data
+
+bob = EncryptingKeypair()
+e = bob.encrypt(data, pubkey=alice.pub_key)
+assert alice.decrypt(e) == data
+print(alice.decrypt(e)) '''
+
+data = b'Hello world'
+alice = EncryptingKeypair()
+bob = EncryptingKeypair()
+ursula = EncryptingKeypair()
+
+e = alice.encrypt(data)
+re_ab = alice.rekey(bob.pub_key)
+print('-------------- alice rekey re_ab')
+print(re_ab)
+
+e_b = ursula.reencrypt(re_ab, e)
+print('-------------- ursula re-encrypt')
+print(e_b)
+
+assert bob.decrypt(e_b) == data
+print('-------------- alice attempt decrypt')
+print(alice.decrypt(e))
+print(bob.decrypt(e_b))
+print(ursula.decrypt(e_b))
+
