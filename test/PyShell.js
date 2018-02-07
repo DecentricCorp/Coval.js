9ac78f20c2b5c6156a3ae6406edb131c5ebcfa69
Timothy Fredette <tim@unspecified.me>
2018-02-07 11:32:52 -0500

    adjust test wording in PyShell.js to be more declarative

--- test/PyShell.js
+++ test/PyShell.js
@@ -7,7 +7,7 @@ let PyShellOptions = PyShellLib.PyShellOptions
 let PyShellMode = PyShellLib.Mode
 
 describe('PyShellOptions', () => {
-    it('is created sucessfully', () => {
+    it('Is created sucessfully', () => {
         var options = new PyShellOptions(PyShellMode.Text, 'path/to/python', ['-u'], 'path/to/my/scripts', ['value1', 'value2', 'value3'] )
         expect(options).to.deep.equal(
             {
@@ -19,7 +19,7 @@ describe('PyShellOptions', () => {
             }
         )
     })
-    it('should execute simple python script', function(done) {    
+    it('Executes a simple python script', function(done) {    
         var pyshell = new PyShell()
         pyshell.Run('test/test.py', function(err, msg){
             expect(msg).to.contain('test execution')
@@ -27,7 +27,7 @@ describe('PyShellOptions', () => {
         })
     })
 
-    it('should execute interactive python script', function(done) {    
+    it('Executes an interactive python script', function(done) {    
         var pyshell = new PyShell('test/interactive.py')
         pyshell.Send('Shannon Code', function(msg){
             expect(msg).to.contain('What is your name?')
