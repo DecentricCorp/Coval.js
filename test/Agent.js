8c90c2b293b5ee6569b1cdf3536404e64b4a0e3b
Timothy Fredette <tim@unspecified.me>
2018-02-08 10:06:31 -0500

    Add additional clarification on some tests as to what it is testing

--- test/Agent.js
+++ test/Agent.js
@@ -39,19 +39,19 @@ describe('Agent', () => {
         expect(agent.user.type).to.equal(UserType.Generic)
     })
 
-    it('allows a server to generate random', function () {
+    it('allows a server to generate a random envelope', function () {
         var agent = new Agent(UserLib.Server)
         var data = agent.user.Generate()
         expect(data).to.exist
     })
 
-    it('allows a server to split random', function () {
+    it('allows a server to split a random envelope', function () {
         var agent = new Agent(UserLib.Server)
         var data = agent.user.Split(2, 2, 256)
         expect(data).to.exist
     })
 
-    it('allows a server to split random and reassemble', function () {
+    it('allows a server to split and reassemble a random envelope', function () {
         var agent = new Agent(UserLib.Server)
         var key = agent.Generate()
         var shares = agent.Split(2, 2, 256)
@@ -59,7 +59,7 @@ describe('Agent', () => {
         expect(combined.value).to.equal(key.value)
     })
 
-    it('prevents a generic agent to generate random', function () {
+    it('prevents a generic agent from generating a random envelope', function () {
         var agent = new Agent()
         expect(function () {
             agent.Generate()
