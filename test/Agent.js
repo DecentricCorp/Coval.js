28bcdf5697302f6a7d9e854b29650d33700150dc
Timothy Fredette <tim@unspecified.me>
2018-02-12 14:36:25 -0500

    Update code based on rebase from current master

--- test/Agent.js
+++ test/Agent.js
@@ -2,18 +2,11 @@
 var UserLib = require('../build/base/User')
 var UserType = UserLib.UserType
 var Agent = require('../build/Agent').Agent
-var Envelope = require('../build/transport/Envelope').Envelope
 var Unloq = require('../build/partner/Unloq').Unloq
 var chai = require('chai')
 var expect = chai.expect
 
 describe('Agent', () => {
-
-    const private_key = '88ab17c73a58992ff6fe06aa5258095acb2f73ff872d73ae6f403eeb1248a007'
-    var server_agent
-    beforeEach(function() {
-        server_agent = new Agent(UserLib.Server)
-    })
 
     it('identifies as a client when cast as client', function () {
         const agent = new Agent(UserLib.Client)
@@ -35,8 +28,9 @@ describe('Agent', () => {
     })
 
     it('identifies as a server when cast as server', function () {
-        expect(server_agent.user.constructor.name).to.equal("Server")
-        expect(server_agent.user.type).to.equal(UserType.Server)
+        const agent = new Agent(UserLib.Server)
+        expect(agent.user.constructor.name).to.equal("Server")
+        expect(agent.user.type).to.equal(UserType.Server)
     })
 
     it('identifies as generic when not cast', function () {
--- test/User.js
+++ test/User.js
@@ -1,4 +1,5 @@
 "use strict"
+var Envelope = require('../build/transport/Envelope').Envelope
 var UserLib = require('../build/base/User')
 var UserType = UserLib.UserType
 var Dat = require('../build/transport/Dat').Dat
@@ -8,45 +9,65 @@ var should = chai.should()
 
 describe('User', function () {
     describe('of type Server', () => {
-         
+
+        const private_key = '88ab17c73a58992ff6fe06aa5258095acb2f73ff872d73ae6f403eeb1248a007'
+        var server
+        beforeEach(function () {
+            server = new UserLib.Server()
+        })
+
         it('generates a random seed', function () {
-            var user = new UserLib.Server()
-            var data = user.Generate()
+            var data = server.Generate()
+            expect(data).to.be.an.instanceOf(Envelope)
             expect(data).to.exist
         })
-        
+
         it('generates shares with no key provided', function () {
-            var user = new UserLib.Server()
-            var data = user.Split(2, 2, 256)
+            var data = server.Split(2, 2, 256)
             expect(data).to.exist
         })
-        
+
         it('reassembles shares into expected seed', function () {
-            var user = new UserLib.Server()
-            var key = user.Generate()
-            var shares = user.Split(2, 2, 256)
-            var combined = user.Combine(shares.value)
+            var key = server.Generate()
+            var shares = server.Split(2, 2, 256)
+            var combined = server.Combine(shares.value)
             expect(combined.value).to.equal(key.value)
         })
-        
-        it('provides a key', function(){
-            var user = new UserLib.Server()
-            var pk = '88ab17c73a58992ff6fe06aa5258095acb2f73ff872d73ae6f403eeb1248a007'
-            user.SetKey(pk)
-            expect(user.key.key).to.equal(pk)
+
+        it('prevents passing a non-number to generate', function () {
+            expect(function () {
+                server.Generate('not a number')
+            }).to.throw(Error, 'Number of bits must be an Integer between 1 and 65536')
         })
-        
-        it('splits a provided key', function(){
-            var user = new UserLib.Server()
-            var pk = '88ab17c73a58992ff6fe06aa5258095acb2f73ff872d73ae6f403eeb1248a007'
-            user.SetKey(pk)
-            var shares = user.Split(2, 2, 256)
-            var combined = user.Combine(shares.value)
-            expect(user.key.key).to.equal(pk)
-            expect(combined.value).to.equal(pk)
+
+        it('provides a key', function () {
+            server.SetKey(private_key)
+            expect(server.GetKey()).to.equal(private_key)
+        })
+
+        it('splits a provided key', function () {
+            server.SetKey(private_key)
+            var shares = server.Split(2, 2, 256)
+            var combined = server.Combine(shares.value)
+            expect(server.GetKey()).to.equal(private_key)
+            expect(combined.value).to.equal(private_key)
+        })
+
+        it('generates only one new key per server instantiation', function () {
+            const first_key = server.Generate()
+            const second_key = server.Generate()
+            expect(first_key.value).to.equal(second_key.value)
+        })
+
+        it('generates different keys with different servers', function () {
+            const first_key = server.Generate()
+            const second_server = new UserLib.Server
+            const second_key = second_server.Generate()
+            expect(first_key.value).to.not.equal(second_key.value)
         })
     })
-    describe('of type Generic', ()=>{
+
+    describe('of type Generic', () => {
         it('can not generate a random seed', function () {
             var user = new UserLib.User()
             expect(function () {
