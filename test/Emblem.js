26146bf9440fdc5894779d56a8dc2274b4bdadc8
Timothy Fredette <tim@unspecified.me>
2018-02-13 16:36:27 -0500

    add test suggestions

--- test/Emblem.js
+++ test/Emblem.js
@@ -25,8 +25,20 @@ describe('Emblem', function () {
             expect(msg1.GetValue()).to.equal('Sucessfully added dat')
             expect(msg2.Errors()[0].message).to.equal('Dat of this type already exists')
         })
+
+        it('allows more than one dat to be added for different dat types')
     })
     describe('HasRequiredDats', function () {
+
+        it('is false when no dats are present')
+        it('is false when only client dat is present')
+        it('is false when only server dat is present')
+        it('is false when only client dat and identity dat are present')
+        it('is false when only client dat and generic dat are present')
+        it('is false when only server dat and identity dat are present')
+        it('is false when only server dat and generic dat are present')
+
+        // Split this test into the tests above
         it('is false when required dats are not fulfilled', function () {
             var emblem = new Emblem()
             var dat = new Dat(UserLib.Client)
@@ -35,6 +47,9 @@ describe('Emblem', function () {
             emblem.AddDat(dat)
             expect(emblem.HasRequiredDats()).to.false
         })
+        
+        it('is true when client and server dats are present') // suggested rewording to below test
+
         it('is true when required dats are fulfilled', function () {
             var emblem = new Emblem()
             var dat1 = new Dat(UserLib.Client)
@@ -45,6 +60,7 @@ describe('Emblem', function () {
         })
     })
     describe('Claimed', () => {
+        // Add code to claim an emblem, even if not yet developed. (can always tweak later to implementation)
         it('new emblem returns false until claimed', () => {
             var emblem = new Emblem()
             expect(emblem.claimed).to.be.false
