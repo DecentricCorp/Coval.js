dafa05dd5b3cfd0aace39052981eec558a3319f2
Timothy Fredette <tim@unspecified.me>
2018-02-15 11:17:49 -0500

    favor const over var

--- test/Emblem.js
+++ test/Emblem.js
@@ -1,10 +1,10 @@
 "use strict"
-var chai = require('chai')
-var expect = chai.expect
-var should = chai.should()
-var UserLib = require('../build/base/User')
-var Emblem = require('../build/Emblem').Emblem
-var Dat = require('../build/transport/Dat').Dat
+const chai = require('chai')
+const expect = chai.expect
+const should = chai.should()
+const UserLib = require('../build/base/User')
+const Emblem = require('../build/Emblem').Emblem
+const Dat = require('../build/transport/Dat').Dat
 
 describe('Emblem', function () {
 
@@ -20,24 +20,24 @@ describe('Emblem', function () {
 
     describe('Add Dat', function () {
         it('successfully adds dat to emblem', function () {
-            var msg = emblem.AddDat(client_dat)
+            const msg = emblem.AddDat(client_dat)
             expect(emblem.dats).to.have.lengthOf(1)
             expect(msg.GetValue()).to.equal('Sucessfully added dat')
         })
         it('only allows a single dat of any type to be added', function () {
-            var dat2 = new Dat(UserLib.Client)
-            var msg1 = emblem.AddDat(client_dat)
-            var msg2 = emblem.AddDat(dat2)
+            const dat2 = new Dat(UserLib.Client)
+            const msg1 = emblem.AddDat(client_dat)
+            const msg2 = emblem.AddDat(dat2)
             expect(emblem.dats).to.have.lengthOf(1)
             expect(msg1.GetValue()).to.equal('Sucessfully added dat')
             expect(msg2.Errors()[0].message).to.equal('Dat of this type already exists')
         })
 
         it('allows more than one dat to be added for different dat types', function () {
-            var client_msg = emblem.AddDat(client_dat)
-            var server_msg = emblem.AddDat(server_dat)
-            var identity_msg = emblem.AddDat(identity_dat)
-            var generic_msg = emblem.AddDat(generic_dat)
+            const client_msg = emblem.AddDat(client_dat)
+            const server_msg = emblem.AddDat(server_dat)
+            const identity_msg = emblem.AddDat(identity_dat)
+            const generic_msg = emblem.AddDat(generic_dat)
             expect(client_msg.GetValue()).to.equal('Sucessfully added dat')
             expect(server_msg.GetValue()).to.equal('Sucessfully added dat')
             expect(identity_msg.GetValue()).to.equal('Sucessfully added dat')
