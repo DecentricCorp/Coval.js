f78f0ef07dba1678e4409e4a43cffe6259b63f3e
Jeremy Gilreath <gilreathjw@Jeremys-MacBook-Pro.local>
2018-08-02 15:19:56 -0400

    reverted .gitignore, build tsc

--- build/secure/ManyKeys.js
+++ build/secure/ManyKeys.js
@@ -1,4 +1,15 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var coininfo_1 = require("coininfo");
 var CoinKey = require("coinkey");
@@ -7,7 +18,7 @@ var bitcoinBitcoreLib = bitcoin.toBitcore();
 var ManyKeys = /** @class */ (function () {
     function ManyKeys(seed) {
         if (seed) {
-            this.seed = new Buffer(seed, 'hex');
+            this.seed = Buffer.from(seed, 'hex');
         }
     }
     ManyKeys.prototype.GenKeys = function () {
@@ -15,33 +26,40 @@ var ManyKeys = /** @class */ (function () {
         return this.ck;
     };
     ManyKeys.prototype.As = function (type, network) {
-        var coin = type;
-        if (network) {
-            coin = coin + '-' + network;
-        }
-        this.ck = new CoinKey(this.seed, coininfo_1.coininfo(coin).versions);
+        this.ck = new CoinKey(this.seed, coininfo_1.coininfo(network ? type + '-' + network : type).versions);
         return this.ck;
     };
     ManyKeys.prototype.GetAllAddresses = function () {
-        var parent = this;
-        var addresses = {};
-        Object.keys(coininfo_1.supportedCoins).forEach(function (_coin) {
-            addresses[_coin] = { address: new CoinKey(parent.seed, coininfo_1.coininfo(coininfo_1.supportedCoins[_coin].name).versions).publicAddress, unit: coininfo_1.supportedCoins[_coin].unit };
-        });
-        return addresses;
+        var _this = this;
+        return Object.keys(coininfo_1.supportedCoins)
+            .map(function (coin) { return _this.createAddressDict(coin); })
+            .reduce(function (address1, address2) { return ManyKeys.combineDicts(address1, address2); });
     };
     ManyKeys.prototype.GetAllKeys = function () {
-        var parent = this;
-        var addresses = {};
-        Object.keys(coininfo_1.supportedCoins).forEach(function (_coin) {
-            var key = new CoinKey(parent.seed, coininfo_1.coininfo(coininfo_1.supportedCoins[_coin].name).versions);
-            addresses[_coin] = { wif: key.privateWif, unit: coininfo_1.supportedCoins[_coin].unit };
-        });
-        return addresses;
+        var _this = this;
+        return Object.keys(coininfo_1.supportedCoins)
+            .map(function (coin) { return _this.createKeyDict(coin); })
+            .reduce(function (key1, key2) { return ManyKeys.combineDicts(key1, key2); });
     };
     ManyKeys.prototype.KeyFromWif = function (wif) {
         var ck = CoinKey.fromWif(wif);
         return { privateKey: ck.privateKey.toString('hex'), address: ck.publicAddress };
+    };
+    ManyKeys.prototype.createAddressDict = function (coin) {
+        var addresses = {};
+        addresses[coin] = { address: this.createCoinKey(coin).publicAddress, unit: coininfo_1.supportedCoins[coin].unit };
+        return addresses;
+    };
+    ManyKeys.prototype.createKeyDict = function (coin) {
+        var keys = {};
+        keys[coin] = { wif: this.createCoinKey(coin).privateWif, unit: coininfo_1.supportedCoins[coin].unit };
+        return keys;
+    };
+    ManyKeys.prototype.createCoinKey = function (coin) {
+        return new CoinKey(this.seed, coininfo_1.coininfo(coininfo_1.supportedCoins[coin].name).versions);
+    };
+    ManyKeys.combineDicts = function (item1, item2) {
+        return __assign({}, item1, item2);
     };
     return ManyKeys;
 }());
