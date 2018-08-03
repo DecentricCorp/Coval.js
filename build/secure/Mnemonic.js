a27c41054da3c61625280e44c8dc0fd27b3d55ea
shannon code <shannoncode@shannons-MBP-2.lan>
2018-08-03 10:56:36 -0400

    Mnemonic

--- build/secure/Mnemonic.js
+++ build/secure/Mnemonic.js
@@ -1,8 +1,39 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var mneumonic = require("bip39");
+var bip39Shamir = require("bip39shamir-web");
 var Mnemonic = /** @class */ (function () {
-    function Mnemonic() {
+    function Mnemonic(seed) {
+        if (seed) {
+            this.seed = Buffer.from(seed, 'hex');
+        }
     }
+    Mnemonic.prototype.Generate = function (strength) {
+        if (this.seed) {
+            return mneumonic.entropyToMnemonic(this.seed);
+        }
+        else {
+            return mneumonic.generateMnemonic(strength || 256);
+        }
+    };
+    Mnemonic.prototype.ToSeedHex = function (phrase) {
+        return mneumonic.mnemonicToSeedHex(phrase);
+    };
+    Mnemonic.prototype.ToEntropy = function (phrase) {
+        if (!phrase) {
+            return "Missing Phrase";
+        }
+        else {
+            return mneumonic.mnemonicToEntropy(phrase);
+        }
+    };
+    Mnemonic.prototype.Split = function (quantity, threshold, phrase) {
+        var shares = bip39Shamir.shares(phrase || this.Generate(128), quantity || 3, threshold || 2);
+        return shares;
+    };
+    Mnemonic.prototype.Combine = function (shares) {
+        return bip39Shamir.combine(shares);
+    };
     return Mnemonic;
 }());
 exports.Mnemonic = Mnemonic;
--- src/secure/Mnemonic.ts
+++ src/secure/Mnemonic.ts
@@ -1,4 +1,42 @@
 "use strict"
+import * as mneumonic from "bip39"
+import * as bip39Shamir from "bip39shamir-web"
+
 export class Mnemonic {
-    constructor() { }    
+    seed: any;
+    constructor(seed?) { 
+        if (seed) {
+            this.seed = Buffer.from(seed, 'hex')
+        }
+    }
+
+    Generate(strength?){
+        if (this.seed) {
+            return mneumonic.entropyToMnemonic(this.seed)
+        } else {
+            return mneumonic.generateMnemonic(strength || 256)
+        }
+    }
+
+    ToSeedHex(phrase){
+        return mneumonic.mnemonicToSeedHex(phrase)
+    }
+
+    ToEntropy(phrase){
+        if (!phrase) {
+            return "Missing Phrase"
+        } else {
+            return mneumonic.mnemonicToEntropy(phrase)
+        }
+    }
+
+    Split(quantity?, threshold?, phrase?){
+        var shares = bip39Shamir.shares(phrase || this.Generate(128), quantity || 3, threshold || 2)
+        return shares
+    }
+
+    Combine(shares) {
+        return bip39Shamir.combine(shares)
+    }
+
 }
